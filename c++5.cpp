3c95af0898611c8a1814eeec09e66da7f51e6877
Your Name <reazulislam137@gmail.com>
2018-05-13 00:10:00 +0600

    first commit

---
+++ c++5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int var;
+int& fun()
+{
+    return var;
+}
+int main()
+{
+    fun() = 10;
+    cout << "Value of var :  "<<var << endl;
+
+    return 0;
+}
